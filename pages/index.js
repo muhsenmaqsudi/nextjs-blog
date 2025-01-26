cd1a7316664b9da08a425265689c00f7f6f9038e
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2025-01-26 12:36:50 +0330

    changed

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Im a software engineer</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
